1587ee452b24cac83470484febfacfc03a9a6ac1
kosticus <kek2009@gmail.com>
2015-08-22 07:07:35 -0500

    first commit

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+var answer = prompt("Please pick a number from 1-100");
+
+function numRequired() {
+	answer = prompt("Please pick a number from 1-100");
+}
+
+if (isNaN(+answer)) {
+	alert("Please supply a number.");
+	numRequired();
+	} else if (answer % 1 != 0) {
+	alert("Please supply a whole number.");
+	numRequired();
+	} 
+
+function fizzbuzz(answer) {
+	for (var i = 1; i <= +answer; i++) {
+			if (i % 15 === 0) {
+			console.log("fizzbuzz");
+			}
+		else if (i % 3 === 0) {
+			console.log("fizz");
+			}
+		else if (i % 5 === 0) {
+			console.log("buzz");
+			} 
+		else {
+			console.log(i);
+			}
+		}
+	}
+
+fizzbuzz(answer);
