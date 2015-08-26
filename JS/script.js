f46c845cda6515d4114c05664a034292854eafb2
kosticus <kek2009@gmail.com>
2015-08-26 08:22:56 -0500

    third commit

--- JS/script.js
+++ JS/script.js
@@ -4,33 +4,30 @@ function numRequired() {
 	answer = prompt("Please pick a number from 1-100");
 }
 
-if (isNaN(+answer)) {
+while (isNaN(+answer)) {
 	alert("Please supply a number.");
 	numRequired();
-	} else if (answer % 1 != 0) {
+}
+while (answer % 1 != 0) {
 	alert("Please supply a whole number.");
 	numRequired();
-	} 
+}
 
-var ul = document.createElement("ul");
-document.getElementById("ouput").appendChild(ul);
 
 function fizzbuzz(answer) {
 	for (var i = 1; i <= +answer; i++) {
-		el = document.createElement("li");
 			if (i % 15 === 0) {
-			el.textContent = "fizzbuzz";
+			console.log("fizzbuzz");
 			}
 		else if (i % 3 === 0) {
-			el.textContent = "fizz";
+			console.log("fizz");
 			}
 		else if (i % 5 === 0) {
-			el.textContent = "buzz";
+			console.log("buzz");
 			} 
 		else {
-			el.textContent = i;
+			console.log(i);
 			}
-			ul.appendChild(el);
 		}
 	}
 
